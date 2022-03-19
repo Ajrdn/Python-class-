944a7a4c21c401e9ab4ef6e158565a98b22064a2
Ajrdn <ajrdn1276@gmail.com>
2022-03-19 18:32:44 +0900

    a, b의 값을 정한 뒤, 사칙연산한 값을 출력하는 코드

---
+++ 2일차/2일차 (3).py
@@ -0,0 +1,13 @@
+# a, b의 값을 정한 뒤, 사칙연산한 값을 출력하는 코드
+
+a = 100
+b = 50
+
+print (a, '+', b, '=', a + b)
+print (a, '-', b, '=', a - b)
+print (b, '-', a, '=', b - a)
+print (a, '*', b, '=', a * b)
+print (a, '/', b, '=', a / b)
+print (b, '/', a, '=', b / a)
+print (a, '%', b, '=', a % b)
+print (b, '%', a, '=', b % a)
