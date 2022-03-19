c3e09172e9b1a23b233cbc9620575de4de9520eb
Ajrdn <ajrdn1276@gmail.com>
2022-03-19 18:33:10 +0900

    두 값을 입력받아, 그 값에 대한 사칙연산값을 출력하는 코드

---
+++ 2일차/2일차 (4).py
@@ -0,0 +1,13 @@
+# 두 값을 입력받아, 그 값에 대한 사칙연산값을 출력하는 코드
+
+a = int(input())
+b = int(input())
+
+print (a, '+', b, '=', a + b)
+print (a, '-', b, '=', a - b)
+print (b, '-', a, '=', b - a)
+print (a, '*', b, '=', a * b)
+print (a, '/', b, '=', a / b)
+print (b, '/', a, '=', b / a)
+print (a, '%', b, '=', a % b)
+print (b, '%', a, '=', b % a)
