f0b859eef3888243187693783e9b76cb5e5a28b2
Ajrdn <ajrdn1276@gmail.com>
2022-03-20 21:53:43 +0900

    입력받은 문자열 중 일부분만 출력하는 코드

---
+++ 3일차/3일차 (09).py
@@ -0,0 +1,12 @@
+# 입력받은 문자열 중 일부분만 출력하는 코드
+
+str = input()
+a = int(input())
+b = int(input())
+
+print (str[a:b])
+print (str[b:a])
+print (str[a:])
+print (str[:a])
+print (str[b:])
+print (str[:b])
