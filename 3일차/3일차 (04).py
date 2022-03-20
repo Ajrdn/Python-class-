a7a253f3fa4520697f2a91be60a34209e951e15e
Ajrdn <ajrdn1276@gmail.com>
2022-03-20 21:51:04 +0900

    두 수를 값으로 비교한 결과와 객체로 비교한 결과를 출력하는 코드

---
+++ 3일차/3일차 (04).py
@@ -0,0 +1,17 @@
+# 두 수를 값으로 비교한 결과와 객체로 비교한 결과를 출력하는 코드
+
+a = float(input())
+b = float(input())
+
+print (f'{a == b}')
+print (f'{a != b}')
+print(f'{a > b}')
+print(f'{a < b}')
+print(f'{a >= b}')
+print(f'{a <= b}')
+print (f'{a is b}')
+print (f'{a is not b}')
+print (f'{a is a}')
+print (f'{a is not a}')
+print (f'{b is b}')
+print (f'{b is not b}')
