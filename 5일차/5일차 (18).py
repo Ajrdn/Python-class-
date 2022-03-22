4e9f8da1af9297bd5e9c8ac7daf3c41b4fe9bce3
Ajrdn <ajrdn1276@gmail.com>
2022-03-22 14:32:46 +0900

    생성된 튜플 속 각 요소에 변수를 할당한 뒤 그 변수들을 출력하는 코드

---
+++ 5일차/5일차 (18).py
@@ -0,0 +1,10 @@
+# 생성된 튜플 속 각 요소에 변수를 할당한 뒤 그 변수들을 출력하는 코드
+
+strs = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9)
+
+print (strs)
+
+strs1, strs2, strs3, strs4, strs5, strs6, strs7, strs8, strs9, strs10 = strs
+
+print (strs1, strs2, strs3, strs4, strs5, strs6, strs7, strs8, strs9, strs10)
+
