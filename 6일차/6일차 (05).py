220c4a84befef5184f2d9dcfbcfe6f82a8aa9da7
Ajrdn <ajrdn1276@gmail.com>
2022-03-23 16:09:44 +0900

    입력받은 딕셔너리의 입력받은 Key의 value를 수정하는 코드

---
+++ 6일차/6일차 (05).py
@@ -0,0 +1,17 @@
+# 입력받은 딕셔너리의 입력받은 Key의 value를 수정하는 코드
+
+dt = {}
+a = int(input())
+
+for i in range(a):
+    key = input()
+    value = input()
+    dt[key] = value
+
+print(dt)
+
+key = input()
+value = input()
+dt[key] = value
+
+print(dt)
